b6cd32a9fc641833600d83211e832a9cbd280288
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-10-28 21:32:10 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2_CasasVictor/DswiiCl2CasasVictorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2_CasasVictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl2CasasVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl2CasasVictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL2_CasasVictor/DswiiCl2CasasVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2_CasasVictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl2CasasVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
