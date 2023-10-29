858f38696582e9dbd5dd8f64d6d8341346d3e7f3
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-10-28 22:24:27 -0500

    aplicacion properties, ControllerExceptionHandler, DswiiCl2CasasVictorApplication,errormessage , ResourceNotFoundException

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2_CasasVictor/exception/ErrorMessage.java
@@ -0,0 +1,18 @@
+package pe.edu.cibertec.DSWII_CL2_CasasVictor.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+public class ErrorMessage {
+
+    private Integer statusCode;
+    private Date dateError;
+    private String message;
+    private String description;
+
+}
+
---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2_CasasVictor/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.DSWII_CL2_CasasVictor.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
+
